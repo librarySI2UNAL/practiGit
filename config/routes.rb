7eff2d35c9891abf6a31aa06815802d9dfac293f
David Beltran <debeltranc@unal.edu.co>
2017-02-12 21:46:21 -0500

    BugFixes with routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
-
 	root to: 'pages#index'
-	get 'aboutUS', 'pages#aboutUS'
+	get 'contactUS'=> 'pages#contactUS'
+	get 'aboutUS' => 'pages#aboutUS'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
